6f6ae0735f2ec7918835178b10c6bc9b0dea0be8
GuiTheDevv <104726381+GuiTheDevv@users.noreply.github.com>
2023-07-24 21:48:19 -0400

    backend completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Firestore, setDoc, doc } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'My Angular App';
 
-  // You can add additional properties and methods for your application here
+  constructor(
+    private firestore: Firestore,
+    private snackBar: MatSnackBar 
+  ) {}
+
+  addEmails(email: string) {
+    const ref = doc(this.firestore, 'emails', email);
+    const emailData = { email: email };
+    return setDoc(ref, emailData)
+      .then(() => {
+        this.snackBar.open('Thank You! Email added successfully', '', {
+          duration: 3000, 
+        });
+      })
+      .catch((error) => {
+        console.error('Error adding email:', error);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
